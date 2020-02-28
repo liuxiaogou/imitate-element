a18a3d7775bf972c9ba63e3890efd64b337570d3
liuxiaogou <397880246@qq.com>
2020-02-28 17:53:47 +0800

    完善form表单

---
+++ src/components/form/fyInput.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <input class="fy-input" :type="type"  :value="value" @input="onInput" v-bind="$attrs" @focus="onFocus" >
+    </div>
+</template> 
+<script>
+export default {
+    name:"fyInput",
+    components: {
+        // New,
+    },
+    // inheritAttrs:false,
+    props: {
+        type: {
+            type: String,
+            default: 'text'
+        },
+        value: {
+            type: String,
+            default: '请输入一些内容吧'
+        },
+    },
+    data() {
+        return {
+            // key: value
+        }
+    },
+    methods: {
+        onInput(e) {
+            this.$emit('input',e.target.value);
+        },
+        onFocus(e){
+            console.log(e);
+             this.$emit('focus',e); 
+        }
+        
+    },
+}
+</script> 
+<style scoped>
+.fy-input{
+    height:20px;
+    margin:5px 0;
+}
+</style>
